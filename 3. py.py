2022bb93ee27d671e425f122c47abad93606d86d
Mahim3133333 <mfarhamim@gmail.com>
2025-08-29 15:19:11 +0300

    Add files via upload

---
+++ 3. py.py
@@ -0,0 +1,5 @@
+b = float(input("Enter a base: "))
+h = float(input("Enter a height: "))
+print(f"the perimeter of the rectangle is {2 * (b + h)} ")
+print(f"the area of the rectangle is {b * h} ")
+
